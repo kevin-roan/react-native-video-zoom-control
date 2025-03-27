7c3c710691272f6e11cb01d7e04ccff6b7ee1381
Kevin Roan <kevin@bpract.com>
2025-03-27 14:53:33 +0530

    fix; known issues

--- src/components/PlayPause/styles.ts
+++ src/components/PlayPause/styles.ts
@@ -1,28 +1,19 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
-    position: 'absolute',
+    position: "absolute",
     top: 0,
     right: 0,
     bottom: 0,
     left: 0,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
     zIndex: 99999,
   },
   playContainer: {
-    width: '10%',
-    alignItems: 'center',
-  },
-  play: {
-  },
-  rewind: {
-    elevation: 10, 
-    shadowColor: 'black', 
-    shadowOffset: { width: 0, height: 4 }, 
-    shadowOpacity: 0.3, 
-    shadowRadius: 6, 
+    width: "10%",
+    alignItems: "center",
   },
 });
